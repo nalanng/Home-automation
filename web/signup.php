884bb79d76b8647f4b6256dd42acff0a3b932cd7
Sıla Hacıalioğlu <sila1176@gmail.com>
2023-05-28 18:07:53 +0300

    Updated the signup page by adding css and headera
    Updated the signup page. CSS codes and header are added.

--- web/signup.php
+++ web/signup.php
@@ -7,6 +7,65 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
+<style>
+body {
+  margin: 0;
+}
+/* header */
+header#header.producer {
+  border-bottom: 1px solid rgba(89, 237, 200, 0.4);
+  width: 100%;
+  left: 0%;
+}
+li {
+  display: inline;
+  list-style: none;
+}
+header#header.producer a {
+  text-decoration: none;
+  font-size: large;
+  color: black;
+  padding: 8px;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+header#header.producer a#logo {
+  padding-left: 0;
+  font-size: xx-large;
+}
+header#header.producer a#signOut {
+  position: absolute;
+  right: 30px;
+  border: 1px solid rgba(89, 237, 200, 0.907);
+  background-color: rgba(89, 237, 200, 0.907);
+  border-radius: 20px;
+  top: 10px;
+}
+.gradient-custom-3 {
+
+background: #84fab0;
+background: -webkit-linear-gradient(to right, rgba(132, 250, 176, 0.5), rgba(143, 211, 244, 0.5));
+
+background: linear-gradient(to right, rgba(132, 250, 176, 0.5), rgba(143, 211, 244, 0.5))
+}
+.gradient-custom-4 {
+background: #84fab0;
+
+background: -webkit-linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1));
+
+background: linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1))
+}
+
+    
+</style>
+<header class="producer" id="header">
+      <ul>
+        <li>
+          <a href="#" id="logo">SNautomation</a>
+        </li>
+        <li><a href="#" id="signOut">Sign Out</a></li>
+      </ul>
+    </header>
 <body>
 <section>
   <div class="mask d-flex align-items-center h-100 gradient-custom-3">
